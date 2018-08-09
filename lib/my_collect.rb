7206d1254b29a1dc9a8c789ec955e9c8da76645e
Jordan Farkas <jordfark@gmail.com>
2018-08-09 23:22:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+# def my_collect(collection)
+# #   new_collection = []
+# #   i = 0 
+# #   while i < collection.length 
+# #   yield collection[i]
+# #     new_collection << collection[i]
+# #     i += 1
+# # end 
+# # new_collection
 
+# end 
 
+def my_collect(array)
+  new_array = []
+  i = 0
+
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+
+  new_array
+end
