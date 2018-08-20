d46314d0f161859fb9f8b23de1b5848270b8d3f6
github username <github email address>
2018-08-20 19:19:46 -0400

    use yield to modify elements in an array

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(languages)
+  i = 0
+  modified_languages = []
 
-
+  while i < languages.length
+    modified_languages << yield(languages[i])
+    i = i + 1
+  end
+  return modified_languages
+end
